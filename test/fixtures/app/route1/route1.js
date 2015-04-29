2bd6aefa95ba0314cdb3d1e17c23ad093f05af0e
Jon Stevens <latchkey@gmail.com>
2015-04-29 10:49:35 -0700

    add dev dep on jspm, install angular, use angular in the tests

--- test/fixtures/app/route1/route1.js
+++ test/fixtures/app/route1/route1.js
@@ -1,8 +1,11 @@
+import angular from 'angular';
+
 import App from '../app';
 import Common from '../common/common';
 
 export default class Route1 {
   constructor() {
     console.log('route1');
+    angular.module();
   }
 };
